0835cf294a25f7243fb78442db23886252021cf5
Cемён <semenkorolkov999@gmail.com>
2024-11-06 17:32:44 +0400

    My first commit - created bootstrap view

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,14 +1,19 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
@@ -17,39 +22,56 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @GetMapping
-    public String showAllUsers(Model model) {
+    public String showAllUsers(Model model, Principal principal) {
         List<User> allUsers = userService.showAllUsers();
+        User user = userService.findUserByUsername(principal.getName());
+        List<Role> roles = roleRepository.findAll();
         model.addAttribute("allUsers", allUsers);
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
         return "allUsers";
     }
 
     @GetMapping("/newUser")
     public String newUser(Model model) {
+        List<Role> roles = roleRepository.findAll();
         model.addAttribute("user", new User());
+        model.addAttribute("roles", roles);
         return "newUser";
     }
 
     @PostMapping("/addUser")
-    public String addUser(@ModelAttribute("user") User user) {
+    public String addUser(@ModelAttribute("user") User user, @RequestParam(value = "roles", required = false) Set<Integer> roleIds) {
+        if (roleIds != null) {
+            Set<Role> roles = roleRepository.findAll().stream()
+                    .filter(role -> roleIds.contains(role.getId()))
+                    .collect(Collectors.toSet());
+            user.setRoles(roles);
+        }
         userService.saveUser(user);
         return "redirect:/admin";
     }
 
     @GetMapping("/editUser")
-    public String editUser(@RequestParam("id") int id, Model model) {
+    public String editUser(@RequestParam("id") Integer id, Model model) {
         User user = userService.showUser(id);
+        List<Role> roles = roleRepository.findAll();
         model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
         return "editUser";
     }
 
     @PostMapping("/updateUser")
-    public String updateUser(@ModelAttribute("user") User user) {
+    public String updateUser(@ModelAttribute("user") User user, @RequestParam("roles") Set<Integer> roles) {
         userService.updateUser(user);
         return "redirect:/admin";
     }
 
-    @GetMapping("/deleteUser")
+    @PostMapping("/deleteUser")
     public String deleteUser(@RequestParam("id") int id) {
         userService.deleteUser(id);
         return "redirect:/admin";
